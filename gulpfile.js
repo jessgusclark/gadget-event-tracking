213accd2a4b72d4ac5c9bbfadca07d51b38c8c6d
Jesse Clark <jesse.clark@unco.edu>
2017-02-08 09:42:39 -0700

    Update gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,8 @@ var rename = require("gulp-rename");
 var mocha = require('gulp-mocha');
 
 //default task:
-gulp.task('default', ['sass', 'scripts', 'test']);
-gulp.task('build', ['sass', 'scripts', 'test', 'watch']);
+gulp.task('default', ['sass', 'scripts', 'test', 'watch']);
+gulp.task('build', ['sass', 'scripts', 'test']);
 
 // watchers:
 gulp.task('watch', function() {
